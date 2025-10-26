7d8da09f08e44700b2bb55d4b1f0895daedf46e7
Beverly Nguyen <beverly893@gmail.com>
2025-10-26 12:52:55 +0900

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/photobooth/',
   plugins: [react(), basicSsl()],
   server: {
     // This makes the server accessible on your local network
